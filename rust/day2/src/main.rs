ae8c7447dbcbf042f3510fd1d4de803281dfd811
Yohan Boujon <yo.boujon@gmail.com>
2023-10-11 17:00:35 +0200

    Added input. Reading file and puting them in a vector.

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -1,8 +1,16 @@
+use std::fs::read_to_string;
 use std::collections::HashMap;
 mod rps;
 
 fn main()
 {
+    // Checking if the file is found.
+    let mut oponent : Vec<String> = read_to_string("input").unwrap().split_whitespace().map(|s| s.to_string()).collect();
+    let mut player = oponent.clone();
+    oponent.retain(|s| s == "A" || s == "B" || s == "C");
+    player.retain(|s| s == "X" || s == "Y" || s == "Z");
+    assert!(oponent.len() == player.len(), "Oponent and Player don't play as many turns.");
+
     let converter : HashMap<char, rps::RockPaperScissors> = rps::get_converter();
     let player = converter.get(&'Y').unwrap().battle(converter.get(&'A').unwrap());
     println!("First battle = {}", player);
