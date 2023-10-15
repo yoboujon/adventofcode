a17e4f9ee5242c18d100dc903008e31d95e992ec
Yohan Boujon <yo.boujon@gmail.com>
2023-10-15 13:44:32 +0200

    Day 4 Second Part

--- rust/day4/src/main.rs
+++ rust/day4/src/main.rs
@@ -22,7 +22,23 @@ fn first_part(str: &String) {
     println!("Final Count: {}", count);
 }
 
+fn second_part(str : &String) {
+    let mut count = 0;
+    let pairs: Vec<Vec<&str>> = str
+        .split("\r\n")
+        .map(|ch| ch.split(",").collect())
+        .collect();
+    for pair in &pairs {
+        if convert(pair, 0,1) >= convert(pair, 1,0) && convert(pair, 0,0) <= convert(pair, 1,1) 
+        {
+            count +=1
+        }
+    }
+    println!("Part 2 Count : {}", count);
+}
+
 fn main() {
     let file_content = read_to_string("input").unwrap();
     first_part(&file_content);
+    second_part(&file_content);
 }
